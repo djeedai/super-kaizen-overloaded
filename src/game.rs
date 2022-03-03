81f10905998d33233e823efb05d8135a79f70c08
Jerome Humbert <djeedai@gmail.com>
2022-03-03 22:00:54 +0000

    Add basic bullet spawning

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,10 @@
-use bevy::{app::CoreStage, asset::AssetStage, input::gamepad::GamepadButtonType, prelude::*};
+use bevy::{
+    app::CoreStage,
+    asset::AssetStage,
+    input::gamepad::GamepadButtonType,
+    pbr::{NotShadowCaster, NotShadowReceiver},
+    prelude::*,
+};
 use bevy_atmosphere::*;
 use bevy_tweening::{lens::*, *};
 use leafwing_input_manager::prelude::*;
@@ -10,7 +16,8 @@ use crate::AppState;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(bevy_atmosphere::AtmospherePlugin { dynamic: true })
+        app.register_type::<PlayerController>()
+            .add_plugin(bevy_atmosphere::AtmospherePlugin { dynamic: true })
             .add_plugin(InputManagerPlugin::<PlayerAction>::default())
             .add_system_set_to_stage(
                 CoreStage::Update,
@@ -18,7 +25,9 @@ impl Plugin for GamePlugin {
             )
             .add_system_set_to_stage(
                 CoreStage::Update,
-                SystemSet::on_update(AppState::InGame).with_system(game_run),
+                SystemSet::on_update(AppState::InGame)
+                    .with_system(game_run)
+                    .with_system(bullet_update),
             );
     }
 }
@@ -29,17 +38,41 @@ enum PlayerAction {
     MoveDown,
     MoveLeft,
     MoveRight,
+    ShootPrimary,
 }
 
-#[derive(Component, Default)]
+#[derive(Component, Reflect)]
+#[reflect(Component)]
 struct PlayerController {
     input_dir: Vec2,
+    primary_cooloff: f32,
+    bullet_texture: Handle<Image>,
+    bullet_mesh: Handle<Mesh>,
+    bullet_material: Handle<StandardMaterial>,
+    primary_fire_delay: f32,
+}
+
+impl Default for PlayerController {
+    fn default() -> Self {
+        PlayerController {
+            input_dir: Vec2::ZERO,
+            primary_cooloff: 0.,
+            bullet_texture: Handle::default(),
+            bullet_mesh: Handle::default(),
+            bullet_material: Handle::default(),
+            primary_fire_delay: 0.084,
+        }
+    }
 }
 
 #[derive(Component)]
 struct Player;
 
+#[derive(Component)]
+struct Bullet(pub Vec3);
+
 fn game_run(
+    mut commands: Commands,
     mut query: Query<(
         &mut PlayerController,
         &ActionState<PlayerAction>,
@@ -50,30 +83,59 @@ fn game_run(
     //println!("game_run");
 
     let (mut controller, action_state, mut transform) = query.single_mut();
+    let dt = time.delta_seconds();
+
     controller.input_dir = Vec2::ZERO;
     if action_state.pressed(&PlayerAction::MoveUp) {
         controller.input_dir.y += 1.;
-        println!("UP");
     }
     if action_state.pressed(&PlayerAction::MoveDown) {
         controller.input_dir.y -= 1.;
-        println!("DOWN");
     }
     if action_state.pressed(&PlayerAction::MoveLeft) {
         controller.input_dir.x -= 1.;
-        println!("LEFT");
     }
     if action_state.pressed(&PlayerAction::MoveRight) {
         controller.input_dir.x += 1.;
-        println!("RIGHT");
     }
     if let Some(input_dir) = controller.input_dir.try_normalize() {
         controller.input_dir = input_dir;
-
-        const SPEED: f32 = 1.;
-        let dv = input_dir * SPEED * time.delta_seconds();
-
+        const SPEED: f32 = 1.6;
+        let dv = input_dir * SPEED * dt;
         transform.translation += Vec3::new(dv.x, dv.y, 0.);
+    }
+
+    let was_cooling = controller.primary_cooloff > 0.;
+    controller.primary_cooloff -= dt;
+    if action_state.pressed(&PlayerAction::ShootPrimary) && controller.primary_cooloff <= 0. {
+        if !was_cooling {
+            controller.primary_cooloff = 0.;
+        }
+        controller.primary_cooloff += controller.primary_fire_delay;
+        commands
+            .spawn_bundle(PbrBundle {
+                mesh: controller.bullet_mesh.clone(),
+                material: controller.bullet_material.clone(),
+                transform: transform.with_rotation(Quat::from_rotation_x(90_f32.to_radians())),
+                ..Default::default()
+            })
+            .insert(NotShadowCaster)
+            .insert(NotShadowReceiver)
+            .insert(Bullet(Vec3::X * 5.));
+    }
+}
+
+fn bullet_update(
+    mut commands: Commands,
+    mut query: Query<(Entity, &mut Transform, &Bullet)>,
+    time: Res<Time>,
+) {
+    let dt = time.delta_seconds();
+    for (entity, mut transform, bullet) in query.iter_mut() {
+        transform.translation += bullet.0 * dt;
+        if transform.translation.distance_squared(Vec3::ZERO) > 120. {
+            commands.entity(entity).despawn();
+        }
     }
 }
 
@@ -84,6 +146,18 @@ fn game_setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
     println!("game_setup");
+
+    let bullet_texture = asset_server.load("textures/bullet1.png");
+    let mut player_controller = PlayerController::default();
+    player_controller.bullet_texture = bullet_texture.clone();
+    player_controller.bullet_mesh = meshes.add(Mesh::from(shape::Plane { size: 0.1 }));
+    player_controller.bullet_material = materials.add(StandardMaterial {
+        base_color_texture: Some(bullet_texture),
+        //emissive: Color::RED,
+        unlit: true,
+        alpha_mode: AlphaMode::Blend,
+        ..Default::default()
+    });
 
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(0.0, 0.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -105,13 +179,20 @@ fn game_setup(
 
     let mut input_map = InputMap::default();
     input_map.insert(PlayerAction::MoveUp, KeyCode::Up);
+    input_map.insert(PlayerAction::MoveUp, KeyCode::W);
     input_map.insert(PlayerAction::MoveUp, GamepadButtonType::DPadUp);
     input_map.insert(PlayerAction::MoveDown, KeyCode::Down);
+    input_map.insert(PlayerAction::MoveDown, KeyCode::S);
     input_map.insert(PlayerAction::MoveDown, GamepadButtonType::DPadDown);
     input_map.insert(PlayerAction::MoveLeft, KeyCode::Left);
+    input_map.insert(PlayerAction::MoveLeft, KeyCode::A);
     input_map.insert(PlayerAction::MoveLeft, GamepadButtonType::DPadDown);
     input_map.insert(PlayerAction::MoveRight, KeyCode::Right);
+    input_map.insert(PlayerAction::MoveRight, KeyCode::D);
     input_map.insert(PlayerAction::MoveRight, GamepadButtonType::DPadDown);
+    input_map.insert(PlayerAction::ShootPrimary, KeyCode::Space);
+    input_map.insert(PlayerAction::ShootPrimary, KeyCode::LControl);
+    //input_map.insert(PlayerAction::ShootPrimary, MouseButton::Left);
 
     commands
         .spawn_bundle(PbrBundle {
@@ -122,7 +203,7 @@ fn game_setup(
         })
         .insert(Name::new("Player"))
         .insert(Player)
-        .insert(PlayerController::default())
+        .insert(player_controller)
         .insert_bundle(InputManagerBundle::<PlayerAction> {
             action_state: ActionState::default(),
             input_map,
